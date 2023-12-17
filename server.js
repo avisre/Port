c6d683933e8a90e2c5bb25a802ba66035235a5a6
avisre <90497762+avisre@users.noreply.github.com>
2023-12-17 11:33:10 +0000

    Add files via upload

---
+++ server.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+// ...
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Serve static files from the 'Front-end' folder
+app.use(express.static(path.join(__dirname, 'Front-end')));
+app.use(cors());
+mongoose.connect('mongodb+srv://project:project@cluster0.kos1k7l.mongodb.net/portfolio1', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+const contactSchema = {
+    name: String,
+    email: String,
+    message: String,
+};
+const Contact = mongoose.model('Contact', contactSchema);
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'Front-end', 'index.html'));
+});
+
+app.post('/add', function (req, res) {
+    let newContact = new Contact({
+        name: req.body.name,
+        email: req.body.email,
+        message: req.body.message,
+    });
+    newContact.save();
+    res.json({ message: 'Form submitted successfully!' });
+    res.redirect('/');
+});
+
+// Add a route to serve the CSS file
+app.get('/style.css', function (req, res) {
+    res.sendFile(path.join(__dirname, 'Front-end', 'style.css'));
+});
+
+app.listen(3000, function () {
+    console.log('App is running on http://localhost:3000');
+});
